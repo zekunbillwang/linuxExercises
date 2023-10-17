3cfc305899d40e986e93d62ab96b9de445a95706
zekunbillwang <zwang2965@wisc.edu>
2023-10-17 13:38:16 -0500

    Update school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,2 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOL" | cut -d ',' -f 7 | { sum=0; i=0; while read n; do sum=$(($sum + $n)); i=$(($i+1)); done; echo "The sum is $sum"; echo "The average is $(($sum/$i))"; } 
